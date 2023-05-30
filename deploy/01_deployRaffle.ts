bf59bb52571d9c239d9cd1badec9ffadc320fc0f
xinbai <whitefusion@163.com>
2023-05-30 22:22:15 +0800

    add deploy script

--- deploy/01_deployRaffle.ts
+++ deploy/01_deployRaffle.ts
@@ -1,5 +1,6 @@
 import { VERIFICATION_BLOCK_CONFIRMATIONS, developmentChains, networkConfig } from '../helper-hardhat-config';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import verify from '../utils/verify';
 
 module.exports = async (hre: HardhatRuntimeEnvironment) => {
   const {getNamedAccounts, deployments, network, ethers} = hre;
@@ -7,21 +8,42 @@ module.exports = async (hre: HardhatRuntimeEnvironment) => {
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
   const currNetworkConfig = networkConfig?.[chainId!] || {};
-  const { vrfCoordinatorV2, entranceFee, gasLane } = currNetworkConfig;
+  const {
+    vrfCoordinatorV2: testNetVrfCoordinatorV2,
+    entranceFee,
+    gasLane,
+    subscriptionId: testNetSubscriptionId,
+     callbackGasLimit,
+    interval
+  } = currNetworkConfig;
   let vrfCoordinatorV2Address, subscriptionId;
+  const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
 
   if (developmentChains.includes(network.name)) {
     const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
-    vrfCoordinatorV2Address = vrfCoordinatorV2Mock;
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const transactionReceipt = await transactionResponse.wait(1);
+    subscriptionId = transactionReceipt.events[0].args.subId;
+    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT);
   } else {
-    vrfCoordinatorV2Address = vrfCoordinatorV2;
+    vrfCoordinatorV2Address = testNetVrfCoordinatorV2;
+    subscriptionId = testNetSubscriptionId;
   }
-
-  const args = [vrfCoordinatorV2Address, entranceFee, gasLane];
+  console.log('deployer: ', deployer);
+  const args = [vrfCoordinatorV2Address, entranceFee, gasLane, subscriptionId, callbackGasLimit, interval]; 
   const raffle = await deploy("Raffle", {
     from: deployer,
-    args: [],
+    args,
     log: true,
     waitConfirmations: VERIFICATION_BLOCK_CONFIRMATIONS
   });
+
+  if ( !developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY ) {
+    log('verifying ...');
+    await verify(raffle.address, args);
+  }
+  log('---------------------------------------------');
 }
+
+module.exports.tags = ['all', 'raffle'];
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -7,6 +7,9 @@ export interface networkConfigItem {
   vrfCoordinatorV2?: string;
   entranceFee?: BigNumber;
   gasLane?: string;
+  subscriptionId?: number;
+  callbackGasLimit?: number;
+  interval?: number;
 }
 export interface networkConfigInfo {
   [key: string]: networkConfigItem,
@@ -16,12 +19,17 @@ export const networkConfig: networkConfigInfo = {
     name: "sepolia",
     vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
     entranceFee: utils.parseEther('0.01'),
-    gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c"
+    gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+    subscriptionId: 0,
+    callbackGasLimit: 500000,
+    interval: 30
   },
   31337: {
     name: "hardhat",
     entranceFee: utils.parseEther('0.01'),
-    gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c"
+    gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+    callbackGasLimit: 500000,
+    interval: 30
   }
 }
 
